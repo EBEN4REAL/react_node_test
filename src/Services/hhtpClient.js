20658a262be859acef0d4016c1a94b230ffc446e
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-01 00:16:11 +0100

    add http client

---
+++ src/Services/hhtpClient.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const get = (url, config) => api.get(url, config);
+export const post = (url, data, config) => api.post(url, data, config);
+export const put = (url, data, config) => api.put(url, data, config);
+export const del = (url, config) => api.delete(url, config);
