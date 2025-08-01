9a3e6e81915eb8959fce78de55714af71cc20925
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-01 05:13:05 +0100

    add user logs endpoint and model

--- server/src/routes/admindash.js
+++ server/src/routes/admindash.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User"); 
+const UserLog = require("../models/UserLog"); 
+const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 
-router.get("/users", async (req, res) => {
+router.get("/users", protect, adminOnly, async (req, res) => {
   try {
     const users = await User.find({}, "-password"); 
     res.json(users);
@@ -13,20 +15,23 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.delete("/users/:email", async (req, res) => {
-  const { email } = req.params;
-
+router.get("/userLogs",  protect, adminOnly, async (req, res) => {
   try {
-    const deletedUser = await User.findOneAndDelete({ email });
-
-    if (!deletedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    
-    res.json({ message: "User deleted successfully" });
+    const logs = await UserLog.find().sort({ loginTime: -1 });
+    res.json(logs);
   } catch (err) {
-    res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error: "Failed to fetch logs" });
   }
 });
+
+router.delete("/userLogs/:id",  protect, adminOnly, async (req, res) => {
+  try {
+    await UserLog.findByIdAndDelete(req.params.id);
+    res.json({ message: "Log deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to delete log" });
+  }
+});
+
 
 module.exports = router;
